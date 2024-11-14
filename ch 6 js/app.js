f5d244f17138657c38b1345229cbb4eb0b04dc1f
laiba khan <laibakhan200416@gmail.com>
2024-11-14 22:07:45 +0500

    Add files via upload

---
+++ ch 6 js/app.js
@@ -0,0 +1,205 @@
+//question 1//
+
+// let city=prompt("enter your city name:");
+// if(city=="karachi"){
+//     document.write("WELCOOME TO THE CITY OF LIGHT")
+// }else{
+//     document.write ("welcome to the :"+ city);
+// }
+
+//question 2//
+
+// let gender=prompt('ENTER YOUR GENDER');
+// if(gender=="male"){
+//     document.write("good morning sir");
+// }else if(gender=="female"){
+//     document.write ("good morning ma'am");
+// }else{
+//     document.write("invalid gender");
+// }
+
+//question 3//
+
+// let input=prompt("Enter traffic light color");
+// if(input=="red"){
+//     document.write("must stop")
+// }else if(input=="yellow"){
+//     document.write ("ready to move");
+
+// }else if(input=="green"){
+//     document.write ("move now")
+
+//     }
+
+//Question 4//
+
+// let currentfuel=prompt("Enter remaining fuel in your car ");
+// if(currentfuel<0.25+"literes"){
+//     alert("please refill the fuel in your car");
+
+// }else{
+//     alert("fuel is sufficient");
+// }
+
+
+//  Question 5//
+
+    //a//
+
+let a=4;
+if(++a ===5){
+    alert("Given condition for variable a is true");
+}
+ //b//
+// let b= 82;
+// if(b++===83){
+//     alert("Given condition for variable b is false");
+// }
+
+ //c//
+
+// let c =12;
+// if(c++===13){
+//     alert("Condition 1 is true");
+// }                         
+// if(c===13){
+//     alert("Condition 2 is true");
+// }
+// if(++c<14){
+//     alert('condition 3 is true');
+// }
+// if(c===14){
+//     alert('condition 4 is true');
+// }
+
+//d//
+// let materialcost= 20000;
+// let laborcost=2000;
+// let totalcost=materialcost+laborcost;
+// if(totalcost=== laborcost+materialcost){
+//     alert("The cost equals");
+// }
+
+//e//
+
+// if(true){
+//     alert("True");
+// }
+// if(false){
+//     alert("false");
+// }
+
+//f//
+
+// if("car"<"cat"){
+//     alert("car is smaller than cat");
+// }document.write("MARKS SHEETS"+"<br>");
+
+
+///QUESTION 6
+
+// let sub1=parseInt(prompt("Enter your English marks obtained"));
+// let sub2=parseInt(prompt("Enter your maths marks obtained"));
+// let sub3=parseInt(prompt("Enter your Islamiate marks obtained"));
+// let totalmarks=prompt("Enter total marks ");
+// let totalmarksobt=sub1+sub2+sub3;
+
+// document.write("MARKS SHEETS"+"<br>");
+// document.write('TOTAL MARKS:',totalmarks+"<br>");
+// document.write("MARKS OBTAINED:",totalmarksobt+"<br>");
+// let percentage=totalmarksobt/totalmarks*100;
+// document.write("PERCENTAGE:",percentage+"%"+"<br>");
+
+// if(totalmarksobt>=80){
+//     document.write("GRADE:A-ONE"+"<BR>");
+//     document.write("REMARKS:EXCELLENT");
+// }else if(totalmarksobt>=70){
+//     document.write("GRADE:A"+"<BR>");
+//     document.write("REMARKS:GOOD");
+// }else if (totalmarksobt>=60){
+//     document.write("GRADE:B"+"<BR>");
+//     document.write("REMARKS:YOU NEED TO IMPROVE");
+// }
+// else{
+//     document.write("GRADE:FAIL");
+//     document.write("REMARKS:SORRY");
+// };
+
+
+//QUESTION 7
+
+// Store a secret number
+// const secretNumber = 5;
+
+// // Prompt user to guess the number
+// const guess = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+
+// // Check the guess
+// if (guess === secretNumber) {
+//     alert("Bingo! Correct answer.");
+// } else if (guess + 1 === secretNumber) {
+//     alert("Close enough to the correct answer.");
+// } else {
+//     alert("Wrong guess. Try again!");
+// }
+
+
+
+ //Question 8
+    //  let num =prompt("ENTER A NUMBER");
+    //  if(num%2!==0){
+    //     alert("THIS NUMBER CAN BE DIVISIBLE BY 3");
+    //  }
+
+
+
+///QUESTION 9
+
+
+// let num =prompt("ENTER A NUMBER");
+// if(num%2==0){
+//     alert("ITS AN EVEN NUMBER");
+//  }
+//      if(num%2!==0){
+//         alert("ITS AN ODD NUMBER");
+//      }
+
+
+
+//QUESTION 10
+// let temperature=prompt("GUIDE ME ABOUT CURRENT TEMPERATURE OF YOUR AREA");
+// if(temperature>40){
+//     document.write("its too hot outside");
+// }
+// else if(temperature>30){
+//     document.write("The weather today is normal");
+// }
+// else if(temperature>20){
+//     document.write("Today's weather is cool");
+// }
+// else if(temperature>10){
+//     document.write("OMG!Today's weather is so cool");
+// }
+
+
+///QUESTION 11
+
+
+// let num1 = parseInt(prompt("Enter first number:"));
+// let num2 = parseInt(prompt("Enter second number:"));
+// let operation = prompt("Enter operation:\n + \n - \n * \n % \n /");
+
+// if (operation === "+") {
+//   document.write(num1 + num2);
+// } else if (operation === "-") {
+//   document.write(num1 - num2);
+// } else if (operation === "*") {
+//   document.write(num1 * num2);
+// } else if (operation === "/")
+//      document.write(num1 /num2);
+//   else if(operation==="%"){
+//             document.write(operation=="%")
+//         }
+     
+  
+
